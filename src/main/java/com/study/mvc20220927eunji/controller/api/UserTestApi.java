d1b6e4d4e98c057903627d9cac70270caf5128f9
OrengYee <orangekoucha@naver.com>
2022-09-29 17:52:39 +0900

    0929 수업 + postman 공부

---
+++ src/main/java/com/study/mvc20220927eunji/controller/api/UserTestApi.java
@@ -0,0 +1,52 @@
+package com.study.mvc20220927eunji.controller.api;
+
+import com.study.mvc20220927eunji.dto.UserReqDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
+
+@ResponseBody
+@Controller
+public class UserTestApi {
+
+    @PostMapping("/api/test/user/dto")
+    public String addUserDto(UserReqDto userReqDto) {
+
+//    Map<String, String> map = new HashMap<String, String>();
+//
+//         map.put("username",username);
+//         map.put("password",password);
+//         map.put("name",name);
+//         map.put("email",email);
+//
+//         System.out.println("map: "+map.toString());
+
+        return userReqDto.toString();
+
+    }
+
+    @ResponseBody
+    @PostMapping("/api/test/user/json")
+    public Object addUserJSON(@RequestBody UserReqDto userReqDto){
+        List<Map<Integer, UserReqDto>> list = new ArrayList<Map<Integer,UserReqDto>>();
+
+        Map<Integer,UserReqDto> map = null;
+
+        for(int i = 0; i < 100; i++){
+            if(i == 0 || i % 10 == 0) {
+                map = new TreeMap<Integer, UserReqDto>();
+            }
+            map.put(i + 1, userReqDto);
+            if((i-9) % 10 == 0){
+                list.add(map);
+            }
+        }
+        return list;
+    }
+}
+
+
--- src/main/java/com/study/mvc20220927eunji/controller/pageController.java
+++ src/main/java/com/study/mvc20220927eunji/controller/pageController.java
@@ -15,20 +15,11 @@ public class pageController {
     public String test1() {
         return "test_page1";
     }
-
-    @GetMapping("/test2")
-    public String test2() {
-        System.out.println("test2");
-        return "test/test_page2";
-    }
-
-    @GetMapping("/test3")
-    public String test3(Model model, @RequestParam String strData, @RequestParam String name) {
-        System.out.println(strData);
-        System.out.println(name);
-
-        model.addAttribute(atti)
-        model.addAttribute(att:"name", name);
-        return "test/test_page3";
-    }
 }
+
+
+
+
+
+
+
